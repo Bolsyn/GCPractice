9f41f0ea497b6a2af287dfc990bf57b14793ad3d
Bolsyn <Serikagis@yandex.ru>
2021-02-16 21:25:58 +0600

    Add project files.

---
+++ GCPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace GCPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.С помощью рефлексии вызвать метод Substring класса String
+            // и извлечь из строки подстроку заданного размера.
+            string text = "Good day";
+
+
+            // 2.Получить список конструкторов класса List<T>
+
+            Type tp = typeof(List<>);
+
+            ConstructorInfo[] constructors = tp.GetConstructors();
+            
+            int i = 0;
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                i++;
+                Console.WriteLine($"Method[{i}] = {constructor.Name}");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
